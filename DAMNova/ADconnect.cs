71ee43f2a75bf53d2fe49b890496ae4948d923d2
Hemi Rei <1019057@techtorium.ac.nz>
2018-11-20 09:21:10 +1300

    Added using to AD class

--- DAMNova/ADconnect.cs
+++ DAMNova/ADconnect.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
 
 namespace DAMNova
 {
