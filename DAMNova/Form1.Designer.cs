97221cca43c369cfb44c0052db74b84b640834e3
Lenore <2021206@techtorium.ac.nz>
2018-11-27 09:31:14 +1300

    Update Form1.Designer.cs

--- DAMNova/Form1.Designer.cs
+++ DAMNova/Form1.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -35,62 +36,62 @@
             this.userNameLabel = new System.Windows.Forms.Label();
             this.passwordLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
+            //
             // userNameBox
-            // 
+            //
             this.userNameBox.Location = new System.Drawing.Point(266, 212);
             this.userNameBox.Name = "userNameBox";
             this.userNameBox.Size = new System.Drawing.Size(249, 20);
             this.userNameBox.TabIndex = 0;
             this.userNameBox.Text = "User Name";
-            // 
+            //
             // passwordBox
-            // 
+            //
             this.passwordBox.Location = new System.Drawing.Point(266, 248);
             this.passwordBox.Name = "passwordBox";
             this.passwordBox.PasswordChar = '*';
             this.passwordBox.Size = new System.Drawing.Size(249, 20);
             this.passwordBox.TabIndex = 1;
             this.passwordBox.Text = "Password";
-            // 
+            //
             // logInLabel
-            // 
+            //
             this.logInLabel.AutoSize = true;
             this.logInLabel.Location = new System.Drawing.Point(350, 161);
             this.logInLabel.Name = "logInLabel";
             this.logInLabel.Size = new System.Drawing.Size(37, 13);
             this.logInLabel.TabIndex = 2;
             this.logInLabel.Text = "Log In";
-            // 
+            //
             // logInButton
-            // 
+            //
             this.logInButton.Location = new System.Drawing.Point(353, 274);
             this.logInButton.Name = "logInButton";
             this.logInButton.Size = new System.Drawing.Size(75, 23);
             this.logInButton.TabIndex = 3;
             this.logInButton.Text = "Log In";
             this.logInButton.UseVisualStyleBackColor = true;
-            // 
+            //
             // userNameLabel
-            // 
+            //
             this.userNameLabel.AutoSize = true;
             this.userNameLabel.Location = new System.Drawing.Point(200, 215);
             this.userNameLabel.Name = "userNameLabel";
             this.userNameLabel.Size = new System.Drawing.Size(60, 13);
             this.userNameLabel.TabIndex = 4;
             this.userNameLabel.Text = "User Name";
-            // 
+            //
             // passwordLabel
-            // 
+            //
             this.passwordLabel.AutoSize = true;
             this.passwordLabel.Location = new System.Drawing.Point(200, 251);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(53, 13);
             this.passwordLabel.TabIndex = 5;
             this.passwordLabel.Text = "Password";
-            // 
+            //
             // LogIn
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 507);
@@ -118,4 +119,3 @@
         private System.Windows.Forms.Label passwordLabel;
     }
 }
-
