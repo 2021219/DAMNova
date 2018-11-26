20722df420eae76e8556e7f6979a29cb8202a42a
math0 <math0@LAPTOP-B52JTSFL>
2018-11-27 11:32:41 +1300

    Added code for Validating Credentials

---
+++ DAMNova/LogIn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAMNova
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+            Home testform = new Home();
+            testform.Show();
+        }
+
+        public bool ValidateCredentials(string domain, string username, string password)
+        {
+            bool valid = false;
+            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                valid = context.ValidateCredentials(username, password);
+            }
+            if (valid == true)
+            {
+                return valid;
+            }
+            else
+            {
+                MessageBox.Show("Error", "Account does not exist");
+                Application.Exit();
+                return valid;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
