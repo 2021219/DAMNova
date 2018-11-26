efa2c35c3e66b4d03302bf0f455b48e66dc8975f
Lenore <2021206@techtorium.ac.nz>
2018-11-27 09:39:15 +1300

    Update Form1.cs

--- DAMNova/Form1.cs
+++ DAMNova/Form1.cs
@@ -12,6 +12,9 @@ namespace DAMNova
     public partial class Form1 : Form
     {
         public Form1()
+        {
+          
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
