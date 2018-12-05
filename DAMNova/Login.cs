f91f3321f78aa09d92d8d0fe28ead92b2b62a2ad
math0 <math0@LAPTOP-B52JTSFL>
2018-12-06 09:07:13 +1300

    Added some login active directory code

--- DAMNova/Login.cs
+++ DAMNova/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.AccountManagement;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,9 +24,51 @@ namespace DAMNova
             testform.Show();
         }
 
+        public bool ValidateCredentials(string domain, string username, string password)
+        {
+            bool valid = false;
+            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                valid = context.ValidateCredentials(username, password);
+            }
+            if (valid == true)
+            {
+                return valid;
+            }
+            else
+            {
+                MessageBox.Show("Error", "Account does not exist");
+                Application.Exit();
+                return valid;
+            }
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, "NOVA.test");
+
+            UserPrincipal user = UserPrincipal.FindByIdentity(ctx, "SomeUserName");
+
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, "Admin");
+
+            if (user != null)
+            {
+                ValidateCredentials("NOVA.test", UserNameBox.Text, PasswordBox.Text);
+
+                if (user.IsMemberOf(group))
+                {
+                    // login as admin
+                }
+                else
+                {
+                    // login as user
+                }
+            }
         }
     }
 }
